e91a75fca65b26215a2168bbb5ac845300e30b78
Mainak Karmakar <iammainak58@gmail.com>
2024-11-22 19:30:40 +0530

    change in vector

--- classs/one.cpp
+++ classs/one.cpp
@@ -1,14 +1,19 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class MyClass{
     public: 
     int students;
     string studentNames;
+    vector<string> allStudentNames;
 
     void printNames(){
         cout << "Students: " << students << endl;
         cout << "StudentsName: " << studentNames;
+        for(auto it : allStudentNames){
+            cout << it << endl;
+        }
     }
 };
 
@@ -18,6 +23,7 @@ int main(){
 
     classEight.students = 8 ;
     classEight.studentNames = "mainak";
+    classEight.allStudentNames = {"hello" , "The hello "};
 
     classEight.printNames();
     return 0;
